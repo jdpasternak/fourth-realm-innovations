f153ea539dd1e0411e4a487bfae5dbbdc79d9f81
Jake Pasternak <jdp.pasternak@gmail.com>
2023-11-19 03:50:49 -0500

    adds navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, ButtonGroup, Container, Paper } from "@mui/material";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import Services from "./components/Services";
+import ScheduleService from "./components/ScheduleService";
+import Contact from "./components/Contact";
+import { useState } from "react";
+import ApplicationContextProvider from "./ContextProvider";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("services");
+
+  const renderCurrentPage = () => {
+    switch (currentPage) {
+      case "home":
+        return <HomePage />;
+      case "services":
+        return <Services />;
+      case "schedule-service":
+        return <ScheduleService />;
+      case "contact":
+        return <Contact />;
+      default:
+        return <HomePage />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApplicationContextProvider>
+      <Box>
+        <Paper component={"nav"} sx={{ p: 2 }}>
+          <ButtonGroup variant="text" aria-label="button group">
+            <Button onClick={() => setCurrentPage("home")}>Home</Button>
+            <Button onClick={() => setCurrentPage("services")}>Services</Button>
+            <Button onClick={() => setCurrentPage("schedule-service")}>
+              Schedule Service
+            </Button>
+            <Button onClick={() => setCurrentPage("contact")}>Contact</Button>
+          </ButtonGroup>
+        </Paper>
+        <Container>{renderCurrentPage()}</Container>
+      </Box>
+    </ApplicationContextProvider>
   );
 }
 
