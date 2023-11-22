918979d558fd5f1eca04f7732bef6f43ac7532fa
Jake Pasternak <jdp.pasternak@gmail.com>
2023-11-21 23:13:16 -0500

    moves Nav to its own component, refactors to handle app context

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import { Box, Button, ButtonGroup, Container, Paper } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import "./App.css";
-import HomePage from "./components/HomePage";
-import Services from "./components/Services";
-import ScheduleService from "./components/ScheduleService";
-import Contact from "./components/Contact";
-import { useState } from "react";
 import ApplicationContextProvider from "./ContextProvider";
+import Nav from "./components/Nav";
+import { useContext } from "react";
+import ApplicationContext from "./Context";
+
+const MainPage = () => {
+  const { currentPage } = useContext(ApplicationContext);
+
+  return <Container>{currentPage}</Container>;
+};
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("services");
-
-  const renderCurrentPage = () => {
-    switch (currentPage) {
-      case "home":
-        return <HomePage />;
-      case "services":
-        return <Services />;
-      case "schedule-service":
-        return <ScheduleService />;
-      case "contact":
-        return <Contact />;
-      default:
-        return <HomePage />;
-    }
-  };
-
   return (
     <ApplicationContextProvider>
       <Box>
-        <Paper component={"nav"} sx={{ p: 2 }}>
-          <ButtonGroup variant="text" aria-label="button group">
-            <Button onClick={() => setCurrentPage("home")}>Home</Button>
-            <Button onClick={() => setCurrentPage("services")}>Services</Button>
-            <Button onClick={() => setCurrentPage("schedule-service")}>
-              Schedule Service
-            </Button>
-            <Button onClick={() => setCurrentPage("contact")}>Contact</Button>
-          </ButtonGroup>
-        </Paper>
-        <Container>{renderCurrentPage()}</Container>
+        <Nav />
+        <MainPage />
       </Box>
     </ApplicationContextProvider>
   );
