763ce970ba7ed5cdc508cb050159c702d002f4bf
Jake Pasternak <jdp.pasternak@gmail.com>
2023-11-21 23:14:09 -0500

    adds intial state variables to context provider

--- src/ContextProvider.js
+++ src/ContextProvider.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
 import ApplicationContext from "./Context";
+import HomePage from "./components/HomePage.js";
+import ScheduleService from "./components/ScheduleService.js";
 
 const ApplicationContextProvider = ({ children }) => {
   const [sharedData, setSharedData] = useState({});
+  // const [currentPage, setCurrentPage] = useState(<HomePage />);
+  const [currentPage, setCurrentPage] = useState(<ScheduleService />);
+  const [isContactPageAlertHidden, setContactPageAlertHidden] = useState(true);
+  const [isContactPageLoading, setContactPageLoading] = useState(false);
+  const [isMessageSent, setMessageSent] = useState(false);
+  const [isContactFormHidden, setContactFormHidden] = useState(false);
 
   const contextValue = {
     sharedData,
     setSharedData,
+    currentPage,
+    setCurrentPage,
+    contactPageData: {
+      isContactPageAlertHidden,
+      setContactPageAlertHidden,
+      isContactPageLoading,
+      setContactPageLoading,
+      isMessageSent,
+      setMessageSent,
+      isContactFormHidden,
+      setContactFormHidden,
+    },
   };
 
   return (
