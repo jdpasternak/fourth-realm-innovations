f1165352d0086492ed398c79ed12d05330172429
Jake Pasternak <jdp.pasternak@gmail.com>
2023-11-29 14:30:28 -0500

    adds scroll to top function

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import { Box, Container } from "@mui/material";
 import "./App.css";
 import ApplicationContextProvider from "./ContextProvider";
 import Nav from "./components/Nav";
-import { useContext } from "react";
-import ApplicationContext from "./Context";
-
-const MainPage = () => {
-  const { currentPage } = useContext(ApplicationContext);
-
-  return <Container>{currentPage}</Container>;
-};
+import "./i18n";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Services from "./components/Services";
+import ScheduleService from "./components/ScheduleService";
+import Contact from "./components/Contact";
+import useScrollToTop from "./useScrollToTop";git add
+import BlogPost from "./components/BlogPost";
+import BlogList from "./components/BlogList";
+import LoginForm from "./components/login/LoginForm";
+import SignUpForm from "./components/login/SignUpForm";
 
 function App() {
   return (
     <ApplicationContextProvider>
-      <Box>
-        <Nav />
-        <MainPage />
-      </Box>
+      <Router>
+        <ScrollToTop />
+        <Box>
+          <Nav />
+          <Container sx={{ my: 2 }}>
+            <Routes>
+              <Route exact path="/" Component={HomePage} />
+              <Route exact path="/services" Component={Services} />
+              <Route
+                exact
+                path="/schedule-service"
+                Component={ScheduleService}
+              />
+              <Route exact path="/contact" Component={Contact} />
+              <Route exact path="/blog" Component={BlogList} />
+              <Route exact path="/blog/:slug" Component={BlogPost} />
+              <Route exact path="/login" Component={LoginForm} />
+              <Route exact path="/sign-up" Component={SignUpForm} />
+            </Routes>
+          </Container>
+        </Box>
+      </Router>
     </ApplicationContextProvider>
   );
 }
+
+const ScrollToTop = () => {
+  useScrollToTop();
+  return null;
+};
 
 export default App;
