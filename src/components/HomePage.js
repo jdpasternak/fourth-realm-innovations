1f7bc4a6d1abc88b354cf3d2078d38120a46a591
Jake Pasternak <jdp.pasternak@gmail.com>
2023-12-01 11:16:14 -0500

    updates homepage to include images, reformats container

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,6 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Box, Typography, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Typography, Button, Container, Paper, Grid } from "@mui/material";
 import { data } from "../static.js";
-import ApplicationContext from "../Context.js";
 import { Link } from "react-router-dom";
 
 const useHomePageData = () => {
@@ -15,33 +14,81 @@ const useHomePageData = () => {
   return homePageData;
 };
 
-const HomePageSection = React.memo(({ section }) => {
-  const handleSetCurrentPage = (component) => {
-    window.scrollTo(0, 0);
-    setCurrentPage(() => component);
+const HomePageSection = React.memo(({ section, i }) => {
+  const ImageGridItem = ({ section, reverse }) => {
+    return (
+      <Grid
+        item
+        lg={5}
+        sx={{ display: "flex", justifyContent: "center", mr: { xs: 0, lg: 2 } }}
+      >
+        <Paper
+          component={"img"}
+          src={`/img/${section.title.replace("?", "")}.png`}
+          sx={{ maxWidth: "100%", mr: { xs: 0, lg: 2 } }}
+        />
+      </Grid>
+    );
   };
 
-  const { setCurrentPage } = useContext(ApplicationContext);
-  return (
-    <Box sx={{ my: 4 }}>
-      <Typography variant="h5" sx={{ mb: 2 }}>
-        {section.title}
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        {section.content}
-      </Typography>
-      {section.button && (
-        <Button
-          variant={section.button.variant}
-          color="primary"
-          disabled={section.button.disabled}
-          component={Link}
-          to={section.button.to}
+  const TextGridItem = ({ section, reverse }) => {
+    return (
+      <Grid item lg={5} sx={{ display: "flex", alignItems: "center" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            mr: { xs: 0, lg: 2 },
+          }}
         >
-          {section.button.text}
-        </Button>
+          <Typography variant="h5" sx={{ mb: 2, textAlign: "justify" }}>
+            {section.title}
+          </Typography>
+          <Typography variant="body1" sx={{ mb: 2, textAlign: "justify" }}>
+            {section.content}
+          </Typography>
+          {section.button && (
+            <Button
+              variant={section.button.variant}
+              color="primary"
+              disabled={section.button.disabled}
+              component={Link}
+              to={section.button.to}
+            >
+              {section.button.text}
+            </Button>
+          )}
+        </Box>
+      </Grid>
+    );
+  };
+  return (
+    // <Box sx={{ my: 4, display: "flex" }}>
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        my: 2,
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: { lg: "row", xs: i % 2 === 0 ? "row" : "row-reverse" },
+      }}
+    >
+      {i % 2 === 0 ? (
+        <>
+          <ImageGridItem section={section} />
+          <TextGridItem section={section} />
+        </>
+      ) : (
+        <>
+          <TextGridItem section={section} reverse />
+          <ImageGridItem section={section} reverse />
+        </>
       )}
-    </Box>
+    </Grid>
+
+    // </Box>
   );
 });
 
@@ -53,26 +100,27 @@ const HomePage = () => {
   }
 
   return (
-    <>
+    <Container>
       {!homePageData ? (
         <Typography variant={"h5"}>Loading...</Typography>
       ) : (
         <Box>
           <Box sx={{ my: 4 }}>
-            <Typography variant="h4" gutterBottom>
+            <Typography variant="h4" gutterBottom textAlign={"center"}>
               {homePageData.heading}
             </Typography>
           </Box>
 
-          {homePageData.sections.map((section) => (
+          {homePageData.sections.map((section, i) => (
             <HomePageSection
               key={section.id || section.title}
               section={section}
+              i={i}
             />
           ))}
         </Box>
       )}
-    </>
+    </Container>
   );
 };
 
